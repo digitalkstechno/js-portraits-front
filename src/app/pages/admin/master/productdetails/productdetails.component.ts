618fbc51686ea2738103a6c09ede65527d88a608
farheen-Digitalks <farheendigitalks@gmail.com>
2026-03-03 16:03:47 +0530

    `Added custom search dropdown functionality to product details component`

--- src/app/pages/admin/master/productdetails/productdetails.component.ts
+++ src/app/pages/admin/master/productdetails/productdetails.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { SHARED_MODULES } from '../../../../constants/sharedModule';
 import { AdminService } from '../../components/service/admin.service';
 import { ItemsService } from '../service/items.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-productdetails',
@@ -10,15 +11,33 @@ import { ItemsService } from '../service/items.service';
   styleUrl: './productdetails.component.css',
 })
 export class ProductdetailsComponent {
+  productForm!: FormGroup;
   page = 1;
   limit = 10;
   searchText = '';
   pagination: any = { page: 1, limit: 10, total: 0, pages: 1 };
   products: any;
+  items: any;
+  showDropdown = false;
+  selectedItemName = '';
 
-  constructor(private service: ItemsService) {}
+  constructor(
+    private service: ItemsService,
+    private fb: FormBuilder,
+  ) {
+    this.productForm = fb.group({
+      item_name: [''],
+      product_name: [''],
+      bill_rate: [0],
+      per_rate: [0],
+      stock_rate: [0],
+      total: [0],
+      remark: [''],
+    });
+  }
 
   ngOnInit() {
+    this.items = [];
     this.service.products$.subscribe({
       next: (res) => {
         console.log('Products', res);
@@ -33,7 +52,52 @@ export class ProductdetailsComponent {
       error: (err) => console.error('Fetch users failed', err),
     });
 
+    this.loadItems();
+
     this.service.searchProducts(this.page, this.limit);
+  }
+
+  masterItems: any;
+  loadItems() {
+    this.service.items$.subscribe({
+      next: (res) => {
+        console.log('items', res);
+        this.masterItems = res.data;
+      },
+      error: (err) => console.error('Fetch users failed', err),
+    });
+    this.service.searchItems(this.page, this.limit);
+  }
+
+  onItemSearch(searchValue: string) {
+    this.selectedItemName = searchValue; // Input ki value update karein
+
+    if (searchValue.length > 0) {
+      this.items = this.masterItems.filter((item: any) =>
+        item.item_name.toLowerCase().includes(searchValue.toLowerCase()),
+      );
+      this.showDropdown = true;
+    } else {
+      this.items = [];
+      this.showDropdown = false;
+    }
+  }
+
+  // 2. Selection Logic: List se item select karna
+  selectItem(item: any) {
+    this.selectedItemName = item.item_name; // Input mein naam set karein
+    this.showDropdown = false; // Dropdown band karein
+
+    // Yaha aap baki fields (Rate, Qty) bhi auto-fill kar sakte hain
+    // console.log('Selected Item ID:', item._id);
+  }
+
+  // 3. Close on Click Outside: Bahar click karne par dropdown band ho jaye
+  @HostListener('document:click', ['$event'])
+  clickout(event: any) {
+    if (!event.target.closest('.custom-search-container')) {
+      this.showDropdown = false;
+    }
   }
 
   // 🔍 Debounced search
