a38060799d42f7bcb19e9ef04e0010356db76570
farheen-Digitalks <farheendigitalks@gmail.com>
2026-03-03 09:53:05 +0530

    `Added dropdown menu for Master option in admin header`

--- src/app/admin/adminheader/adminheader.component.ts
+++ src/app/admin/adminheader/adminheader.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { SHARED_MODULES } from '../../constants/sharedModule';
 
 @Component({
   selector: 'app-adminheader',
-  imports: [],
+  imports: [SHARED_MODULES],
   templateUrl: './adminheader.component.html',
-  styleUrl: './adminheader.component.css'
+  styleUrl: './adminheader.component.css',
 })
 export class AdminheaderComponent {
+  isMasterOpen = false;
 
+  toggleMaster() {
+    this.isMasterOpen = !this.isMasterOpen;
+  }
+
+  closeMaster() {
+    this.isMasterOpen = false;
+  }
 }
