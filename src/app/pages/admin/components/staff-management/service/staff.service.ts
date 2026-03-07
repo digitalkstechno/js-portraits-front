0cd7010a8b56d28f0e0adc7a9b8f69ba04335deb
farheen-Digitalks <farheendigitalks@gmail.com>
2026-03-07 10:57:26 +0530

    `Refactor staff entry component: add form group, update HTML and TS files`

---
+++ src/app/pages/admin/components/staff-management/service/staff.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Endpoints } from '../../../../../API/api.config';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class StaffService {
+  constructor(private http: HttpClient) {}
+
+  getStaff(): Observable<any> {
+    return this.http.get<any>(Endpoints.STAFF);
+  }
+
+  getStaffById(id: string): Observable<any> {
+    return this.http.get<any>(`${Endpoints.STAFF}/${id}`);
+  }
+
+  getStaffCount(): Observable<any> {
+    return this.http.get<any>(`${Endpoints.STAFF}/count`);
+  }
+
+  createStaff(data: any): Observable<any> {
+    return this.http.post<any>(Endpoints.STAFF, data);
+  }
+}
--- src/app/pages/admin/components/staff-management/staffentry/staffentry.component.ts
+++ src/app/pages/admin/components/staff-management/staffentry/staffentry.component.ts
@@ -1,14 +1,69 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
+import { StaffService } from '../service/staff.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { SHARED_MODULES } from '../../../../../constants/sharedModule';
 
 @Component({
   selector: 'app-staffentry',
-  imports: [],
+  imports: [SHARED_MODULES],
   templateUrl: './staffentry.component.html',
   styleUrl: './staffentry.component.css',
 })
 export class StaffentryComponent {
   router = inject(Router);
+  staffService = inject(StaffService);
+  fb = inject(FormBuilder);
+  staffForm!: FormGroup;
+  totalStaff = 0;
+  activeStaff = 0;
+  onLeaveStaff = 0;
+
+  ngOnInit() {
+    this.staffForm = this.fb.group({
+      staffId: [''],
+      fullName: [''],
+      joiningDate: [new Date().toISOString().split('T')[0]],
+      contactNo: [''],
+      salary: [''],
+      role: [''],
+      address: [''],
+      remarks: [''],
+      status: ['Active'],
+    });
+  }
+
+  onSubmit() {
+    if (this.staffForm.invalid) return;
+
+    const staffData = this.staffForm.value;
+    console.log('Staff Data:', staffData);
+
+    this.staffService.createStaff(staffData).subscribe({
+      next: () => {
+        console.log('Staff created successfully');
+      },
+      error: (err) => {
+        console.error(err);
+      },
+    });
+  }
+
+  updateStaff() {
+    console.log('Update staff');
+  }
+
+  clearForm() {
+    this.staffForm.reset();
+  }
+
+  deleteStaff() {
+    console.log('Delete staff');
+  }
+
+  searchStaff(value: string) {
+    console.log('Searching:', value);
+  }
 
   close() {
     this.router.navigateByUrl('/admin');
