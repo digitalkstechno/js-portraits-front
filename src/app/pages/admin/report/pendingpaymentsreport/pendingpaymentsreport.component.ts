6c04030579ae9606831f242dd6d95df0d4ab869b
farheen-Digitalks <farheendigitalks@gmail.com>
2026-03-14 10:43:55 +0530

    `Added CSS styles and HTML templates for Pending Payments Report component`

--- src/app/pages/admin/report/pendingpaymentsreport/pendingpaymentsreport.component.ts
+++ src/app/pages/admin/report/pendingpaymentsreport/pendingpaymentsreport.component.ts
@@ -1,16 +1,28 @@
 import { Component, inject } from '@angular/core';
 import { AdminService } from '../../components/service/admin.service';
 import { SHARED_MODULES } from '../../../../constants/sharedModule';
+import { IndiancurrencyPipe } from '../../pipe/indiancurrency.pipe';
 
 @Component({
   selector: 'app-pendingpaymentsreport',
-  imports: [SHARED_MODULES],
+  imports: [SHARED_MODULES, IndiancurrencyPipe],
   templateUrl: './pendingpaymentsreport.component.html',
   styleUrl: './pendingpaymentsreport.component.css',
 })
 export class PendingpaymentsreportComponent {
   pendingList: any[] = [];
   outdoorService = inject(AdminService);
+  pendingSummary: any;
+  outstandingList: any[] = [];
+  staffList: any[] = [];
+  totalOutstanding = 0;
+  totalSalaryPending = 0;
+  pendingCategories = {
+    orders: false,
+    bills: false,
+    sales: false,
+    salaries: false,
+  };
 
   ngOnInit() {
     this.loadPendingSummary();
@@ -19,6 +31,39 @@ export class PendingpaymentsreportComponent {
   loadPendingSummary(sDate?: string, eDate?: string) {
     this.outdoorService.getPendingAmountSummary(sDate, eDate).subscribe({
       next: (res) => {
+        this.pendingSummary = res.reportData;
+        // 1. Outstanding List Prepare karein (Orders, Bills, Sells ka mix)
+        this.outstandingList = [
+          { name: 'Outdoor Orders', ...this.pendingSummary.orders },
+          { name: 'Outdoor Bills', ...this.pendingSummary.bills },
+          { name: 'Product Sales', ...this.pendingSummary.sells },
+        ];
+
+        // 2. Staff List
+        this.staffList = this.pendingSummary.staffStatus;
+
+        // 3. Summary Cards Calculations
+        this.totalOutstanding =
+          this.pendingSummary.orders.totalPending +
+          this.pendingSummary.bills.totalPending +
+          this.pendingSummary.sells.totalPending +
+          this.staffList.reduce(
+            (acc: number, curr: any) => acc + curr.pendingAmount,
+            0,
+          );
+        this.totalSalaryPending = this.staffList.reduce(
+          (acc: number, curr: any) => acc + curr.pendingAmount,
+          0,
+        );
+        this.pendingCategories.orders =
+          this.pendingSummary.orders.totalPending > 0;
+        this.pendingCategories.bills =
+          this.pendingSummary.bills.totalPending > 0;
+        this.pendingCategories.sales =
+          this.pendingSummary.sells.totalPending > 0;
+        this.pendingCategories.salaries = this.staffList.some(
+          (s) => s.pendingAmount > 0,
+        );
         console.log(res);
         // Force Angular to detect changes before initializing charts
         // this.cdr.detectChanges();
